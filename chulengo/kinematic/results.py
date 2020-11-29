f5a67c58bd03c30ba621579b10fdab1717c1121c
Marian Domanski <mmdski@gmail.com>
2020-11-29 14:14:02 -0600

    implement recorder and computation results

--- chulengo/kinematic/results.py
+++ chulengo/kinematic/results.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
 
 
-class Results:
+class ResultsRecorder:
 
     def __init__(self, spatial_domain, t_initial, q_initial):
 
@@ -13,31 +14,6 @@ class Results:
     def add_results(self, t, q):
 
         self._results[t] = q
-
-    def results_array(self):
-
-        results = np.empty((self.n_times(), 1, self.n_cells()))
-        times = self.times()
-
-        for i, t in enumerate(times):
-            results[i, :, :] = self._results[t]
-
-        return results
-
-    def times(self):
-
-        keys = list(self._results.keys())
-        keys.sort()
-
-        return np.array(keys, dtype=np.float)
-
-    def n_times(self):
-
-        return len(self._results)
-
-    def n_cells(self):
-
-        return self._n_cells
 
     def plot(self, t=None, axes=None):
 
@@ -60,6 +36,64 @@ class Results:
 
         return axes
 
+    def results_array(self):
+
+        results = np.empty((self.n_times(), self.n_cells()))
+        times = self.times()
+
+        for i, t in enumerate(times):
+            results[i, :] = self._results[t]
+
+        return results
+
+    def times(self):
+
+        keys = list(self._results.keys())
+        keys.sort()
+
+        return np.array(keys, dtype=np.float)
+
+    def n_times(self):
+
+        return len(self._results)
+
+    def n_cells(self):
+
+        return self._n_cells
+
     def x(self):
 
+        return self._x.copy()
+
+
+class ComputationResults:
+
+    def __init__(self, results_recorder, dt):
+
+        recorded_results = results_recorder.results_array()
+        recorded_times = results_recorder.times()
+
+        total_time = recorded_times[-1] - recorded_times[0]
+        n_times = int(total_time/dt)
+
+        times = dt*np.arange(n_times, dtype=float) + recorded_times[0]
+
+        interp_func = interp1d(recorded_times, recorded_results, axis=0)
+        results_array = interp_func(times)
+
+        self._dt = dt
+        self._x = results_recorder.x()
+        self._times = times
+        self._array = results_array
+
+    def array(self):
+        return self._array.copy()
+
+    def dt(self):
+        return self._dt
+
+    def times(self):
+        return self._times.copy()
+
+    def x(self):
         return self._x.copy()
